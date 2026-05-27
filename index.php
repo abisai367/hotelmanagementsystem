eda8c3aac3707b596c5bbafe0c2f5213723849fa
Abisai367 <janeingweti@gmail.com>
2026-05-27 17:05:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
+header("Access-Control-Allow-Origin: https://abisai367.github.io");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("HTTP/1.1 200 OK");
     exit();
 }
 
